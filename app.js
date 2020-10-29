a37a0853d104ee1fc831aaa587472489098a37d0
ezhang120 <ezhang120@gmail.com>
2020-10-29 18:44:49 -0400

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=MYTOKENHERE";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,42 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+    handleResponse(request.response);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const handleResponse = (requestResponse) => {
+  const jsonified = JSON.parse(requestResponse);
+  const plantsArray = jsonified.data;
+
+  plantNames = plantsArray.filter((arrayItem) => {
+    return arrayItem.year > 1753;
+  })
+
+  const myPlants = plantNames.map(arrayItem => arrayItem.common_name);
+  addPlants(myPlants);
+  
+  console.log(myPlants);
+}
+
+// const displayDiv = () => {
+//   const wrapperDiv = document.createElement("div");
+//   wrapperDiv.setAttribute("id", "wrapper");
+//   const header = document.createElement("h1");
+//   header.innerText = "Plants from 1753";
+//   wrapperDiv.appendChild(header);
+//   document.getElementById("plants").appendChild(wrapperDiv);
+// }
+
+const addPlants = (myPlants) => {
+  const myDiv = document.createElement("div");
+  myDiv.setAttribute("id", "wrapper");
+  const plant = document.createElement("h3");
+  const header = document.createElement("h1");
+  header.innerText = "Plants from 1753:";
+  plant.innerText = myPlants[0] + '\n' + myPlants[1] + '\n' + myPlants[2];
+  myDiv.appendChild(header);
+  myDiv.appendChild(plant);
+  document.getElementById("plants").appendChild(myDiv);
+}
